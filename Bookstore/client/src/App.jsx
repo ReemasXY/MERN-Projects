dec6684a2cca37a3be700863c0c19181e8126b98
ReemasXY <sameermhr345@gmail.com>
2023-11-22 13:03:54 +0545

    added login and signup (using tailwind)

--- Bookstore/client/src/App.jsx
+++ Bookstore/client/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginLayout from "./layout/LoginLayout";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/authentication" element={<LoginLayout></LoginLayout>}>
+            <Route path="/authentication/login" element={<Login />}></Route>
+            <Route path="/authentication/signup" element={<Signup />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
