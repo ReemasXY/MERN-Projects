194a1ab666fba53d4a998302938053a336748cd4
ReemasXY <sameermhr345@gmail.com>
2025-02-15 09:24:07 +0545

    changes

---
+++ Ecommerce/client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Hello this is the revision</div>
+    </>
+  );
+}
+
+export default App;
